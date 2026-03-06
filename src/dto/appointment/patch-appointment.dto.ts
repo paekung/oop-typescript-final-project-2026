9940c7fdb0c25ceb51e534f631aef8c840715be2
68011128_Sahapoom Rattanawiwatpong <68011128@kmitl.ac.th>
2026-03-07 00:52:18 +0700

    fix dto appointment

--- src/dto/appointment/patch-appointment.dto.ts
+++ src/dto/appointment/patch-appointment.dto.ts
@@ -3,12 +3,12 @@ import { ApiPropertyOptional } from '@nestjs/swagger';
 import { AppointmentStatus } from '../../enums/appointment-status.enum';
 
 export class PatchAppointmentDto {
-  @ApiPropertyOptional({ enum: AppointmentStatus, description: 'สถานะการจองที่ต้องการเปลี่ยน' })
+  @ApiPropertyOptional({ enum: AppointmentStatus, description: 'The new status of the appointment' })
   @IsEnum(AppointmentStatus)
   @IsOptional()
   status?: AppointmentStatus;
 
-  @ApiPropertyOptional({ example: 'ติดธุระด่วน', description: 'เหตุผลการยกเลิก' })
+  @ApiPropertyOptional({ example: 'Urgent personal matters', description: 'Reason for canceling the appointment' })
   @IsString()
   @IsOptional()
   cancellationReason?: string;
