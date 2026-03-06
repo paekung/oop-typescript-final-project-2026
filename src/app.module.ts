b70a6efc6d242a846d3fbccc121ddff709ee5d06
paekung <supakit.3t@gmail.com>
2026-03-07 00:31:50 +0700

    feat(app): prepare AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServiceController } from './controllers/service.controller';
+import { AppointmentController } from './controllers/appointment.controller';
+import { ServiceEntity } from './entities/service.entity';
+import { AppointmentEntity } from './entities/appointment.entity';
+import { ServiceService } from './services/service.service';
+import { AppointmentService } from './services/appointment.service';
 
 @Module({
   imports: [],
